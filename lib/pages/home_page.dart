36e9725ded47f0355bba2882d81b28e899c25825
sgc93 <smachewgedefaw@gmail.com>
2023-09-15 22:32:08 -0900

    construct the homepage of the app

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  late double _deviceWidth, _deviceHeight;
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight * 0.15,
+        title: const Text(
+          'timely',
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+      ),
+      body: _taskList(),
+    );
+  }
+
+  Widget _taskList(){
+    return Container(
+      child: ListView(
+        children: [
+          Card(
+            child: ListTile(
+              title: const Text('Go to church'),
+              subtitle: Text(DateTime.now().toString()),
+              trailing: const Icon(
+                Icons.check_box_outlined,
+                color: Colors.red,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+}
