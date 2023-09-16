0d52a15d05618b3d316b0e0610d3860eecd7ca04
sgc93 <smachewgedefaw@gmail.com>
2023-09-16 14:04:22 -0900

    display tasks from hive db

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:timely/models/task.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +14,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late double _deviceWidth, _deviceHeight;
   String? _newTask;
+  Box? _box;
 
   @override
   Widget build(BuildContext context) {
@@ -34,18 +37,19 @@ class _HomePageState extends State<HomePage> {
 
   Widget _displayTask(){
     return FutureBuilder(
-      future: _fetchImageData(),
+      future: Hive.openBox('tasks'),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if(snapshot.connectionState == ConnectionState.waiting){
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
+        if(snapshot.connectionState == ConnectionState.done){
+          _box = snapshot.data;
+          return _taskList();
         } else if( snapshot.hasError ){
           return Center(
             child: Text(snapshot.error.toString()),
           );
         } else {
-          return _taskList();
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         }
       },
     );
@@ -58,20 +62,30 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _taskList(){
+    // add sample task
+    // Task newTask = Task(content: "Finish at least One project.", timestamp: DateTime.now(), done: true);
+    // _box?.add(newTask.toMap());
+    // get stored tasks
+    List tasks = _box!.values.toList();
     return Container(
-      child: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              title: const Text('Go to church'),
-              subtitle: Text(DateTime.now().toString()),
-              trailing: const Icon(
-                Icons.check_box_outlined,
-                color: Colors.red,
+      child: ListView.builder(
+        itemCount: tasks.length, // _tasks.length,
+        itemBuilder: (BuildContext context, int index) {
+          var task = Task.fromMap(tasks[index]);
+          return ListTile(
+            title: Text(
+              task.content,
+              style: TextStyle(
+                decoration: task.done ? TextDecoration.lineThrough : null,
               ),
             ),
-          )
-        ],
+            subtitle: Text(task.timestamp.toString()),
+            trailing: Icon(
+              task.done? Icons.check_box_outlined : Icons.check_box_outline_blank_outlined,
+              color: Colors.red,
+            ),
+          );
+        },
       ),
     );
   }
